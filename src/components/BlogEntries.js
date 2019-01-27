3ed67c186a4bb406d75d9b9aa3de360b2ad380d1
Mykola Pelekh <mpelekh@lohika.com>
2019-01-27 16:25:14 +0200

    Create BlogPost component

--- src/components/BlogEntries.js
+++ src/components/BlogEntries.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import BlogPost from './BlogPost'
 
 class BlogEntries extends Component {
   render() {
@@ -8,67 +9,37 @@ class BlogEntries extends Component {
           Page Heading <small>Secondary Text</small>
         </h1>
 
-        <div className="card mb-4">
-          {/* <img className="card-img-top" src="http://placehold.it/750x300" alt="Card image cap"> */}
-          <div className="card-body">
-            <h2 className="card-title">Post Title</h2>
-            <p className="card-text">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a
-              laboriosam. Dicta expedita corporis animi vero voluptate
-              voluptatibus possimus, veniam magni quis!
-            </p>
-            <a href="#" className="btn btn-primary">
-              Read More &rarr;
-            </a>
-          </div>
-          <div className="card-footer text-muted">
-            Posted on January 1, 2017 by
-            <a href="#">Start Bootstrap</a>
-          </div>
-        </div>
+        <BlogPost
+          title="Post Title"
+          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a
+            laboriosam. Dicta expedita corporis animi vero voluptate
+            voluptatibus possimus, veniam magni quis!"
+          date="January 1, 2017"
+          author="Mykola Pelekh"
+        />
 
-        <div className="card mb-4">
-          {/* <img className="card-img-top" src="http://placehold.it/750x300" alt="Card image cap"> */}
-          <div className="card-body">
-            <h2 className="card-title">Post Title</h2>
-            <p className="card-text">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a
-              laboriosam. Dicta expedita corporis animi vero voluptate
-              voluptatibus possimus, veniam magni quis!
-            </p>
-            <a href="#" className="btn btn-primary">
-              Read More &rarr;
-            </a>
-          </div>
-          <div className="card-footer text-muted">
-            Posted on January 1, 2017 by
-            <a href="#">Start Bootstrap</a>
-          </div>
-        </div>
+        <BlogPost
+          title="Post Title"
+          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a
+            laboriosam. Dicta expedita corporis animi vero voluptate
+            voluptatibus possimus, veniam magni quis!"
+          date="January 1, 2017"
+          author="Mykola Pelekh"
+        />
 
-        <div className="card mb-4">
-          {/* <img className="card-img-top" src="http://placehold.it/750x300" alt="Card image cap"> */}
-          <div className="card-body">
-            <h2 className="card-title">Post Title</h2>
-            <p className="card-text">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a
-              laboriosam. Dicta expedita corporis animi vero voluptate
-              voluptatibus possimus, veniam magni quis!
-            </p>
-            <a href="#" className="btn btn-primary">
-              Read More &rarr;
-            </a>
-          </div>
-          <div className="card-footer text-muted">
-            Posted on January 1, 2017 by
-            <a href="#">Start Bootstrap</a>
-          </div>
-        </div>
+        <BlogPost
+          title="Post Title"
+          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a
+            laboriosam. Dicta expedita corporis animi vero voluptate
+            voluptatibus possimus, veniam magni quis!"
+          date="January 1, 2017"
+          author="Mykola Pelekh"
+        />
 
-        <ul className="pagination justify-content-center mb-4">
+        <ul className="pagination justify-description-center mb-4">
           <li className="page-item">
             <a className="page-link" href="#">
               &larr; Older
---
+++ src/components/BlogPost.js
@@ -0,0 +1,32 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+
+class BlogPost extends PureComponent {
+  static propTypes = {
+    title: PropTypes.string,
+    description: PropTypes.string,
+    date: PropTypes.string,
+    author: PropTypes.string
+  }
+
+  render() {
+    const { title, description, date, author } = this.props
+
+    return (
+      <div className="card mb-4">
+        <div className="card-body">
+          <h2 className="card-title">{title}</h2>
+          <p className="card-text">{description}</p>
+          <a href="#" className="btn btn-primary">
+            Read More &rarr;
+          </a>
+        </div>
+        <div className="card-footer text-muted">
+          Posted on {date} by <a href="#">{author}</a>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default BlogPost
