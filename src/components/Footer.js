0b49d6609c23994e49e7bed68e1ee0e206542f6c
Mykola Pelekh <mpelekh@lohika.com>
2019-01-28 23:37:47 +0200

    Update css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React, { PureComponent } from 'react'
 class Footer extends PureComponent {
   render() {
     return (
-      <footer className="py-5 bg-dark">
+      <footer className="py-5 bg-dark mt-auto">
         <div className="container">
           <p className="m-0 text-center text-white">
             Copyright &copy; Your Website 2019
