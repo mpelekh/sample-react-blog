b904ff180aa32d9a953d399432a5fba34dcf9397
Mykola Pelekh <mpelekh@lohika.com>
2019-10-22 13:40:01 +0300

    Updates

---
+++ precache-manifest.a4ed866b1418211b6eab417ed732f0b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9c5c64f1018eb3a5592",
+    "url": "/sample-react-blog/static/js/runtime~main.c9c5c64f.js"
+  },
+  {
+    "revision": "97b13cbadcaf25438070",
+    "url": "/sample-react-blog/static/js/main.97b13cba.chunk.js"
+  },
+  {
+    "revision": "9e22086e4128f484527e",
+    "url": "/sample-react-blog/static/js/1.9e22086e.chunk.js"
+  },
+  {
+    "revision": "97b13cbadcaf25438070",
+    "url": "/sample-react-blog/static/css/main.b870ae93.chunk.css"
+  },
+  {
+    "revision": "9e22086e4128f484527e",
+    "url": "/sample-react-blog/static/css/1.38a64d3f.chunk.css"
+  },
+  {
+    "revision": "4a3be726069b9b3c19c4d903d5978b52",
+    "url": "/sample-react-blog/index.html"
+  }
+];
